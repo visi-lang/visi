caf3146680d1f1f5a65164f64971a9249129c0af
David Pollak <feeder.of.the.bears@gmail.com>
2012-01-26 15:07:09 -0800

    Snurping through objc header files, looking for class names

--- objc/Hoe.hs
+++ objc/Hoe.hs
@@ -6,6 +6,9 @@ import Foreign.C.String
 import Foreign.Ptr
 import Foreign.Storable
 import Foreign.Marshal.Alloc
+import System.Directory
+import Data.List
+import Text.Regex.Posix
 
 frog :: CInt -> CInt
 frog n = 
@@ -27,6 +30,41 @@ foreign import ccall safe "objc_lookUpClass" objc_lookUpClass :: CString -> IO O
 foreign import ccall safe "objc_getClassList" objc_getClassList :: Ptr VoidPtr -> CInt -> IO CInt
 foreign import ccall safe "class_getName" class_getName :: VoidPtr -> IO CString
 
+rootDir = "/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS5.0.sdk/System/Library/Frameworks/"
+
+fileList filterFunc root = 
+  if (isSuffixOf "/." root) || (isSuffixOf "/.." root) then
+    return []
+  else
+    do
+      isFile <- doesFileExist root
+      if isFile then
+        return $ filter filterFunc [root]
+      else
+        do
+          isDir <-  doesDirectoryExist root
+          if isDir then
+            do
+              path <- canonicalizePath root
+              kids' <- getDirectoryContents path
+              let kids = map (\name -> path ++ "/" ++ name) kids'
+              subPaths <- mapM (fileList filterFunc) kids
+              return $ concat subPaths
+
+          else
+            return []
+
+findThem :: String -> [String]
+findThem str =
+  case str =~ "\\@interface +([A-Za-z0-9]+) +\\:" :: (String, String, String, [String]) of
+    (_, _, _, ret) -> ret
+
+allClassNames =
+  do
+    files <- fileList (isSuffixOf ".h") rootDir
+    programText <- mapM readFile files
+    let matches = concat $ map findThem programText
+    putStrLn $ "found: " ++ show matches
 
 makeAString :: IO ObjCId
 makeAString = do
@@ -62,6 +100,7 @@ classList =
 
 main = 
 	do
+    allClassNames
     makeAString
     classList
     putStrLn "I'm the Haskell Objective-C Exchange"
