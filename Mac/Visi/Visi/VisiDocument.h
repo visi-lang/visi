cb07eea906480a233917e2252b093d1a9eec378f
David Pollak <feeder.of.the.bears@gmail.com>
2012-02-09 12:23:30 -0800

    Added the framework of an xcode project

---
+++ Mac/Visi/Visi/VisiDocument.h
@@ -0,0 +1,13 @@
+//
+//  VisiDocument.h
+//  Visi
+//
+//  Created by David Pollak on 2/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface VisiDocument : NSDocument
+
+@end
