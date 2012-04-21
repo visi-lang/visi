8dbca2f266e18fdca300184195bcceb2fa63d974
David Pollak <feeder.of.the.bears@gmail.com>
2012-04-20 18:11:01 -0700

    Updated the build script to dynamically create the list of dependencies

--- Mac/Visi/my_cabal.sh
+++ Mac/Visi/my_cabal.sh
@@ -7,10 +7,41 @@ export PATH=$PATH:~/Library/Haskell/bin
 
 DIST="$TARGET_BUILD_DIR/dist"
 CABAL=cabal
-HASKELL_LIB=/Library/Frameworks/GHC.framework/Versions/7.4.1-x86_64/usr/lib/ghc-7.4.1
-USER_LIB=${HOME}/Library/Haskell/ghc-7.4.1/lib/
+# HASKELL_LIB=/Library/Frameworks/GHC.framework/Versions/7.4.1-x86_64/usr/lib/ghc-7.4.1
+# USER_LIB=${HOME}/Library/Haskell/ghc-7.4.1/lib/
 
 
+function depends_on( )
+{
+  if [ "$1" == 'builtin_rts' ] ; then
+    return
+  fi
+  
+  local from=$1
+  local known=$2
+  if [ "`expr \"$known\" : \"$from\"`" != "0" ] ; then
+    return
+  fi
+  local known="$from\n$known"
+  local all=$(ghc-pkg field $from depends | grep -o " .*" | grep -o "[^ ]*$" | grep -o "[^-]*.[^-]*" | grep -v "^-")
+  local x=""
+  for x in $all ; do
+    known="${known}\n$(depends_on $x $known)"
+  done
+  local k2=""
+  for x in $known ; do
+    k2="$k2\n$x"
+  done
+  
+  echo $k2 | sort | uniq
+}
+
+pkg_ver () {
+  local p=$1
+  local n=$(ghc-pkg field $p name | grep -o "[^ ]*$")
+  local v=$(ghc-pkg field $p version | grep -o "[^ ]*$")
+  echo "${n}-${v}"
+}
 
 case "$ACTION" in
   "")
@@ -18,21 +49,63 @@ case "$ACTION" in
     $CABAL --extra-include-dirs="$(pwd)/Visi" --extra-include-dirs="$(pwd)/Visi/include" --distpref="$DIST" configure || exit 1
     $CABAL --distpref="$DIST" --verbose build || exit 1
     rm -f WholeVisi.o
-    libtool -v -static -o WholeVisi.o $DIST/build/HSVisi-0.1.o -L${HASKELL_LIB}/pretty-1.1.1.0 -L${USER_LIB}parsec-3.1.2/lib \
-    -L${USER_LIB}uuid-1.2.5/lib -L${USER_LIB}random-1.0.1.1/lib \
-    -L${USER_LIB}utf8-string-0.3.7/lib -L${USER_LIB}SHA-1.5.0.1/lib \
-    -L${USER_LIB}/c2hs-0.16.3/lib -L${USER_LIB}text-0.11.1.13/lib \
-    -L${USER_LIB}mtl-2.0.1.0/lib -L${USER_LIB}transformers-0.2.2.0/lib -L${HASKELL_LIB}/containers-0.4.2.1 -L${HASKELL_LIB}/deepseq-1.3.0.0 \
-    -L${HASKELL_LIB}/bytestring-0.9.2.1 -L${HASKELL_LIB}/array-0.4.0.0 -L${HASKELL_LIB}/base-4.5.0.0 -L${HASKELL_LIB}/integer-gmp-0.4.0.0 \
-    -L${HASKELL_LIB}/time-1.4 -L${HASKELL_LIB}/binary-0.5.1.0 \
-    -L${HASKELL_LIB}/ghc-prim-0.2.0.0 -L${HASKELL_LIB} -lHSpretty-1.1.1.0 -lHSparsec-3.1.2 -lHStext-0.11.1.13 -lHSmtl-2.0.1.0 \
-    -lHStransformers-0.2.2.0 -lHScontainers-0.4.2.1 -lHSdeepseq-1.3.0.0 \
-    -lHSSHA-1.5.0.1 -lHSutf8-string-0.3.7 \
-    -lHSbytestring-0.9.2.1 -lHSarray-0.4.0.0 -lHSbase-4.5.0.0 \
-    -lHSrandom-1.0.1.1 -lHSuuid-1.2.5 -lHStime-1.4 -lHSbinary-0.5.1.0 \
-    -lHSinteger-gmp-0.4.0.0 -lHSghc-prim-0.2.0.0 -lHSrts_thr 2>&1 | grep -v "libtool: warning same member name"  || exit 1
+
+    if [ $(diff  $DIST/build/autogen/cabal_macros.h  $DIST/build/autogen/cabal_macros.h_cache 2>&1 | wc -l) == "0" ] ; then
+     echo "using cached libtool thing"
+    else
+        root=$(cat $DIST/build/autogen/cabal_macros.h | grep package | grep -o '[^ ]* \*' | grep -o '[^ ]*')
+
+        dep=""
+        dep2=""
+
+        for x in $root; do
+          dep2=$(depends_on "$x" "$dep")
+          dep3=""
+          for y in $dep2; do
+            dep3="${dep3}\n${y}"
+          done
+          dep=$(echo $dep3 | sort | uniq)
+        done
+
+        d=""
+
+        dep_rts="rts ${dep}"
+
+        for x in ${dep_rts}; do
+            z=$(ghc-pkg field $x library-dirs | grep -o "[^ ]*$")
+            q=$z
+            d="$d\n$q"
+        done
+
+        bigl=""
+
+        for x in $(echo $d | sort | uniq); do
+            bigl="${bigl} -L${x}"
+        done
+
+        pkgs=""
+        for x in $dep; do
+           pkgs="${pkgs}\n$(pkg_ver $x)"
+        done
+
+        small=""
+
+        for x in $(echo $pkgs | sort | uniq); do
+            small="${small} -lHS${x}"
+        done
+
+        cp $DIST/build/autogen/cabal_macros.h $DIST/build/autogen/cabal_macros.h_cache
+
+        echo "libtool -v -static -o WholeVisi.o $DIST/build/HSVisi-0.1.o $bigl $small -lHSrts_thr 2>&1 | (grep -v 'libtool: warning same member name')" >  $DIST/build/autogen/cabal_link_cmd.sh
+
+    fi
+
+    sh $DIST/build/autogen/cabal_link_cmd.sh || exit 1
+
     ;;
   clean)
+    rm -f $DIST/build/autogen/cabal_macros.h_cache
+    rm -f $DIST/build/autogen/cabal_link_cmd.sh
     $CABAL --distpref="$DIST" clean || exit 1
     ;;
   *)
