4ca3364695410e0a16d732accda16b610bd8e4bb
David Pollak <feeder.of.the.bears@gmail.com>
2012-12-22 05:03:42 -0800

    Building the dependency graph

--- core/scala/src/main/scala/visi/core/Typer.scala
+++ core/scala/src/main/scala/visi/core/Typer.scala
@@ -13,7 +13,7 @@ object Typer {
   private case class Ref[T](var it: T)
 
   private type TypePtr = Ref[Type]
-  private type VarScope = Map[FuncName, TypePtr]
+  // private type VarScope = Map[FuncName, Expression]
   private case class CurState(refs: ConcurrentHashMap[Type, TypePtr])
   private case class Depends(whatId: LetId, what: Expression, dependants: Set[LetId], predicates: Set[LetId])
   private type LetMap = ConcurrentHashMap[LetId, Depends]
@@ -21,22 +21,34 @@ object Typer {
   def infer(in: Expression): Box[(String, Type)] = {
     val graph = new LetMap
 
-    buildGraph(graph, in)
+    buildGraph(graph, Map.empty, Nil,
+      in)
 
     Empty
   }
 
-  private def buildGraph(stuff: LetMap, exp: Expression) {
-    exp match {
+  private def buildGraph(stuff: LetMap, scope: LetScope, stack: List[Expression], theExp: Expression) {
+    theExp match {
     case LetExp(loc: SourceLoc, id: LetId, name: FuncName, generic: CanBeGeneric, tpe: Type, exp: Expression) =>
+      val newMap = scope + (name -> theExp)
+      stuff.put(id, Depends(id, theExp, Set.empty, Set.empty))
+      buildGraph(stuff, newMap, theExp :: stack, exp)
+
     case InnerLet(loc: SourceLoc,
     tpe: Type, exp1: Expression, exp2: Expression) =>
+
     case SinkExp(loc: SourceLoc,
     id: LetId, name: FuncName, tpe: Type, exp: Expression) =>
+      stuff.put(id, Depends(id, theExp, Set.empty, Set.empty))
+      val newMap = scope + (name -> theExp)
+      buildGraph(stuff, newMap, theExp :: stack, exp)
+
     case SourceExp(loc: SourceLoc,
     id: LetId,
     name: FuncName,
     tpe: Type) =>
+      stuff.put(id, Depends(id, theExp, Set.empty, Set.empty))
+
     case InvokeMethod(loc: SourceLoc,
     id: LetId,
     name: FuncName,
@@ -52,10 +64,18 @@ object Typer {
     tpe: Type,
     exp1: Expression,
     exp2: Expression) =>
-    case Var(loc: SourceLoc, name: FuncName, tpe: Type) =>
-    case BuiltIn(loc: SourceLoc, name: FuncName, tpe: Type, _) =>
+
+    case Var(loc: SourceLoc, id: LetId, name: FuncName, tpe: Type) =>
+
+    case BuiltIn(loc: SourceLoc, id: LetId, name: FuncName, tpe: Type, _) =>
+      stuff.put(id, Depends(id, theExp, Set.empty, Set.empty))
+
     case ValueConst(loc: SourceLoc, value: Value, tpe: Type) =>
+      // Do Nothing
+
     case Group(loc: SourceLoc, map: Map[FuncName, Expression], tpe: Type, exp: Expression) =>
+        val newMap = map.foldLeft(scope){case (sc, (n, e2)) => sc + (n -> e2)}
+        map.values.foreach(e2 => buildGraph(stuff, newMap, theExp:: stack, e2))
     }
   }
 
@@ -74,10 +94,10 @@ object Typer {
 
   private def calcType(scope: VarScope, state: CurState, nonGen: Boolean, expr: Expression): Box[TypePtr] =
   expr match {
-    case Var(_, name, tpe) =>
+    case Var(_, _, name, tpe) =>
       for {
         t <- scope.get(name) ?~ ("Unable to locate function "+name)
-        ret <- fresh(state, nonGen, t)
+        ret <- fresh(state, nonGen, Ref(t))
       } yield ret
     case x => ParamFailure("Unable to calculate the type for ", x)
   }
