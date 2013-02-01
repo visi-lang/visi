60a7eee04635b9b9d1cc8dd844eff2ea417cfd75
David Pollak <feeder.of.the.bears@gmail.com>
2013-02-01 15:30:36 -0800

    Emit code that looks objective-c-ish

--- core/scala/src/main/scala/visi/Main.scala
+++ core/scala/src/main/scala/visi/Main.scala
@@ -1,7 +1,45 @@
 package visi
 
+import core.{Visi, Compiler}
+import net.liftweb.util.Helpers
+import net.liftweb.util.Helpers._
+
+import net.liftweb.common.{Failure, Full}
+
 object Main {
   def main(argv: Array[String]) {
-    println("Dude... ")
+    val str = new String(Helpers.readWholeStream(System.in), "UTF-8")
+    Visi.compile(str) match {
+      case Full(prog) =>
+        val strList: List[String] = (Compiler.jsLibrary + "\n\n" + prog+"\n\n").split("\n").toList
+        System.out.print("@")
+        strList.foreach(s => {
+          System.out.print("\"")
+          System.out.print(fixStr(s))
+          System.out.println("\\n\"")
+
+        })
+        System.out.println(";")
+
+      case Failure(msg, _, _) => System.err.println(msg)
+      case _ => System.err.println("Failed for no good reason")
+    }
+  }
+
+  private def fixStr(in: String): String = {
+    val ret = new StringBuffer(in.length + 20)
+
+    val len = in.length
+    var pos = 0
+    while (pos < len) {
+      in.charAt(pos) match {
+        case '"' => ret.append("\\\"")
+        case '\\' => ret.append("\\\\")
+        case c => ret.append(c)
+      }
+      pos += 1
+    }
+
+    ret.toString
   }
 }
